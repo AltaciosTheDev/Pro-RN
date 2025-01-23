784cfd5791844d520ed5b1f694359aac6d0e0b11
AltaciosTheDev <enzo.altamirano98@gmail.com>
2025-01-22 19:34:16 -0600

    p4-corpcomment-project_structure

---
+++ P4-CORPCOMMENT/src/components/HashtagList.tsx
@@ -0,0 +1,10 @@
+
+export default function HashtagList() {
+  return (
+    <ul className="hashtags">
+      <li><button>#Bytegrad</button></li>
+      <li><button>#Nike</button></li>
+      <li><button>#Mcdonalds</button></li>
+    </ul>
+  )
+}
