f1e451ee4c7a7f230deb06508a02721df0ea16b3
AltaciosTheDev <enzo.altamirano98@gmail.com>
2025-02-01 17:25:10 -0600

    custom hook for fetching specific job item

--- P5-JOBS/src/components/App.tsx
+++ P5-JOBS/src/components/App.tsx
@@ -11,27 +11,14 @@ import ResultsCount from "./ResultsCount";
 import SortingControls from "./SortingControls";
 import JobList from "./JobList";
 import PaginationControls from "./PaginationControls";
-import { useJobItems } from "../lib/hooks";
-import { useEffect, useState } from "react";
+import { useActiveId, useJobItem, useJobItems } from "../lib/hooks";
+import { useState } from "react";
 
 function App() {
   const [searchText, setSearchText] = useState("");
-  const [jobItems, isLoading] = useJobItems(searchText)
-  const [activeId, setActiveId] = useState<number | null>(null)
-  
-  useEffect(() => {
-    const handleHashChange = () => {
-      const id = +window.location.hash.slice(1)//converts to number
-      setActiveId(id)
-    }
-    handleHashChange()
-
-    window.addEventListener("hashchange",handleHashChange)
-
-    return () => {
-      window.removeEventListener("hashchange", handleHashChange)
-    }
-  },[])
+  const [jobItems, isLoading] = useJobItems(searchText) //custom hook 
+  const activeId = useActiveId() //custom hook 
+  const jobItem = useJobItem(activeId)//custom hook 
 
   return (
     <>
@@ -52,7 +39,7 @@ function App() {
           <JobList jobItems={jobItems} isLoading={isLoading}/>
           <PaginationControls />
         </Sidebar>
-        <JobItemContent />
+        <JobItemContent jobItem={jobItem}/>
       </Container>
       <Footer />
     </>
--- P5-JOBS/src/lib/hooks.ts
+++ P5-JOBS/src/lib/hooks.ts
@@ -1,28 +1,88 @@
 import { useEffect, useState } from "react";
 import { JobItem } from "./types";
+import { BASE_API_URL } from "./constants";
 
 //can re use all logic related to getting the jobitems and the loading
 export function useJobItems(searchText: string){
+    //state to store jobs
     const [jobItems, setJobItems] = useState<JobItem[]>([]);//cast when the inferred can get you in trouble(never[]) 
     const [isLoading, setIsLoading] = useState(false);//no need to type b/c ts infers
 
+    //first page
     const jobItemsSliced = jobItems.slice(0,7)
 
+    //side effects here
     useEffect(() => {
+        //set loading
         setIsLoading(true);
     
+        //guard clase for empty form
         if (!searchText) return; //only fetch when input not empty will avoid fetch onloading b/c of this.
     
+        //get data
         const fetchData = async () => {
           const response = await fetch(
-            `https://bytegrad.com/course-assets/projects/rmtdev/api/data?search=${searchText}`
+            `${BASE_API_URL}?search=${searchText}`
           );
           const data = await response.json();
           setIsLoading(false);
           setJobItems(data.jobItems);
         };
         fetchData();
+      //call again if form changes
       }, [searchText]);
 
     return [jobItemsSliced, isLoading] as const //leaves no room for interpretation
+}
+
+export function useActiveId() {
+
+  //state to store id
+    const [activeId, setActiveId] = useState<number | null>(null)
+    
+    //a tag will store id in url
+    //listen and react when the hash changes
+    useEffect(() => {
+      const handleHashChange = () => {
+        const id = +window.location.hash.slice(1)//converts to number
+        //store new value in state
+        setActiveId(id)
+      }
+      handleHashChange()
+
+      //call for function
+      window.addEventListener("hashchange",handleHashChange)
+  
+      //clean this event listener
+      return () => {
+        window.removeEventListener("hashchange", handleHashChange)
+      }
+    },[])
+
+  return activeId
+}
+
+export function useJobItem(activeId: number | null) {
+  //state to store the job
+  const [jobItem, setJobItem] = useState(null)
+    
+    //guard clause if id is null
+    useEffect(() => {
+      if(!activeId) return //remember guard clauses when state can be null initially
+  
+      //get data
+      const fetchData = async () => {
+    
+        const response = await fetch(`${BASE_API_URL}/${activeId}`)
+        const data = await response.json()
+        //store data
+        setJobItem(data.jobItem)
+  
+      }
+      fetchData()
+  
+    //call again is activeId changes
+    },[activeId])//when comp first mounts and when activeId changes
+
+    return jobItem
 }
