166d35b459b80e15df03f83398aecc9b010e4495
AltaciosTheDev <enzo.altamirano98@gmail.com>
2025-02-21 22:15:11 -0600

    1: Children pattern implemented & typing children prop

--- P5-JOBS/src/components/App.tsx
+++ P5-JOBS/src/components/App.tsx
@@ -2,34 +2,60 @@ import { useEffect, useState } from "react";
 import Background from "./Background";
 import Container from "./Container";
 import Footer from "./Footer";
-import Header from "./Header";
+import Header, { HeaderTop } from "./Header";
+import Logo from "./Logo";
+import BookmarksButton from "./BookmarksButton";
+import SearchForm from "./SearchForm";
+import Sidebar, { SidebarTop } from "./Sidebar";
+import JobItemContent from "./JobItemContent";
+import ResultsCount from "./ResultsCount";
+import SortingControls from "./SortingControls";
+import PaginationControls from "./PaginationControls";
+import JobList from "./JobList";
 
 function App() {
-  const [searchText, setSearchText] = useState<string>("")
-  const [jobItems, setJobItems] = useState([])
+  const [searchText, setSearchText] = useState<string>("");
+  const [jobItems, setJobItems] = useState([]);
 
   useEffect(() => {
-    if(!searchText) return
+    if (!searchText) return;
 
     const fetchJobItems = async () => {
-      const response = await fetch(`https://bytegrad.com/course-assets/projects/rmtdev/api/data?search=${searchText}`)
-      if(!response.ok){
-        throw new Error()
+      const response = await fetch(
+        `https://bytegrad.com/course-assets/projects/rmtdev/api/data?search=${searchText}`
+      );
+      if (!response.ok) {
+        throw new Error();
       }
-      const data = await response.json()
-      console.log(data.jobItems)
-      setJobItems(data.jobItems)
-    }
-    fetchJobItems()
-
-  },[searchText])
+      const data = await response.json();
+      console.log(data.jobItems);
+      setJobItems(data.jobItems);
+    };
+    fetchJobItems();
+  }, [searchText]);
 
   return (
     <>
       <Background />
-      <Header searchText={searchText}  setSearchText={setSearchText} />
-      <Container jobItems={jobItems} />
-      <Footer/>
+      <Header>
+        <HeaderTop>
+          <Logo />
+          <BookmarksButton />
+        </HeaderTop>
+        <SearchForm searchText={searchText} setSearchText={setSearchText} />
+      </Header>
+      <Container>
+        <Sidebar>
+          <SidebarTop>
+            <ResultsCount />
+            <SortingControls />
+          </SidebarTop>
+          <JobList jobItems={jobItems} />
+          <PaginationControls />
+        </Sidebar>
+        <JobItemContent />
+      </Container>
+      <Footer />
     </>
   );
 }
