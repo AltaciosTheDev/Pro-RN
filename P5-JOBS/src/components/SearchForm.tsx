6142d5fec9b6f8d4b46025d91cdcfc8c182b2f6c
AltaciosTheDev <enzo.altamirano98@gmail.com>
2025-02-21 20:21:46 -0600

    1: Search Component(Controlled Input)

--- P5-JOBS/src/components/SearchForm.tsx
+++ P5-JOBS/src/components/SearchForm.tsx
@@ -1,13 +1,28 @@
+import { useState } from "react";
 
 export default function SearchForm() {
+  const [searchText, setSearchText] = useState<string>("")
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    alert("Form submitted");
+  };
+
+  const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(`fetching ${e.target.value}`)
+    setSearchText(e.target.value)
+    
+  }
 
   return (
-    <form action="#" className="search">
+    <form onSubmit={handleSubmit} action="#" className="search">
       <button>
         <i className="fa-solid fa-magnifying-glass"></i>
       </button>
 
       <input
+        value={searchText}
+        onChange={handleOnChange}
         spellCheck="false"
         type="text"
         required
