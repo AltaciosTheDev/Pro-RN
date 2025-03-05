64042f0adaccbb30aa2e2ba667d7a6f6938f086f
AltaciosTheDev <enzo.altamirano98@gmail.com>
2025-03-04 21:38:12 -0600

    5: derived last page state and determined the slice of jobitems based on the current page

--- P5-JOBS/src/components/App.tsx
+++ P5-JOBS/src/components/App.tsx
@@ -11,34 +11,35 @@ import ResultsCount from "./ResultsCount";
 import SortingControls from "./SortingControls";
 import PaginationControls from "./PaginationControls";
 import JobList from "./JobList";
-import {useDebounce, useJobItems } from "../lib/hooks";
-import {useState } from "react";
+import { useDebounce, useJobItems } from "../lib/hooks";
+import { useState } from "react";
 import { ITEMS_PER_PAGE } from "../lib/constants";
 import { Toaster } from "react-hot-toast";
-
 
 function App() {
   //states
   const [searchText, setSearchText] = useState<string>("");
-  const debouncedSearchText = useDebounce<string>(searchText)
-  const [jobItems, isLoading] = useJobItems(debouncedSearchText)
-  const [currentPage, setCurrentPage] = useState(1)
+  const debouncedSearchText = useDebounce<string>(searchText);
+  const [jobItems, isLoading] = useJobItems(debouncedSearchText);
+  const [currentPage, setCurrentPage] = useState(1);
 
   //derived states
-  const jobItemsSliced = jobItems?.slice(0 ,ITEMS_PER_PAGE) || []
-  const jobItemsCount = jobItems?.length || 0
-  const lastPage = jobItemsCount / ITEMS_PER_PAGE
-  console.log(currentPage)
+  const jobItemsSliced =
+    jobItems?.slice(
+      currentPage * ITEMS_PER_PAGE - ITEMS_PER_PAGE,
+      currentPage * ITEMS_PER_PAGE
+    ) || [];
+  const jobItemsCount = jobItems?.length || 0;
+  const lastPage = jobItemsCount / ITEMS_PER_PAGE;
+  
   //handlers
-  const handleChangePage = (pagination: 'previous' | 'next') => {
-    if(pagination === 'previous'){
-      setCurrentPage((prev) => prev - 1 )
-      
+  const handleChangePage = (pagination: "previous" | "next") => {
+    if (pagination === "previous") {
+      setCurrentPage((prev) => prev - 1);
+    } else if (pagination === "next") {
+      setCurrentPage((prev) => prev + 1);
     }
-    else if(pagination === 'next'){
-      setCurrentPage((prev) => prev + 1)
-    }
-  }
+  };
 
   return (
     <>
@@ -53,16 +54,20 @@ function App() {
       <Container>
         <Sidebar>
           <SidebarTop>
-            <ResultsCount jobItemsCount={jobItemsCount}/>
+            <ResultsCount jobItemsCount={jobItemsCount} />
             <SortingControls />
           </SidebarTop>
-          <JobList jobItems={jobItemsSliced} isLoading={isLoading}/>
-          <PaginationControls lastPage={lastPage} currentPage={currentPage} onChangePage={handleChangePage}/>
+          <JobList jobItems={jobItemsSliced} isLoading={isLoading} />
+          <PaginationControls
+            lastPage={lastPage}
+            currentPage={currentPage}
+            onChangePage={handleChangePage}
+          />
         </Sidebar>
-        <JobItemContent/>
+        <JobItemContent />
       </Container>
       <Footer />
-      <Toaster position="top-right"/>
+      <Toaster position="top-right" />
     </>
   );
 }
