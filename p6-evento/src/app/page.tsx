d92d3ee4c5f0e2345b42841c994926163a2f4da7
AltaciosTheDev <enzo.altamirano98@gmail.com>
2025-03-11 21:05:45 -0600

    2:Home Page structrue

--- p6-evento/src/app/page.tsx
+++ p6-evento/src/app/page.tsx
@@ -1,7 +1,22 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     Home Page
+    <main className="">
+      <h1>Find events around you</h1>
+      <p>
+        Browse more than <span>10,000 events worldwide</span>
+      </p>
+      <form>
+        <input placeholder="Search events in any city" spellCheck={false}></input>
+      </form>
+      <section>
+        <p>Popular:</p>
+        <div>
+        <Link href={"/events/austin"}>Austin</Link>
+        <Link href={"/events/seattle"}>Seattle</Link>
+        </div>
+      </section>
     </main>
-  )
+  );
 }
