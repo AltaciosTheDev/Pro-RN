82dd9da45e3a0ed65ded3777ada4d54533d43839
AltaciosTheDev <enzo.altamirano98@gmail.com>
2025-03-12 20:25:40 -0600

    2: Home Page styling

--- p6-evento/src/app/components/Footer.tsx
+++ p6-evento/src/app/components/Footer.tsx
@@ -1,7 +1,26 @@
+import Link from 'next/link'
 import React from 'react'
+
+const routes = [
+  {
+    name: "Terms & Conditions",
+    path: "/terms-conditions"
+  },
+  {
+    name: "Privacy Policy",
+    path: "/privacy-policy"
+  }
+]
 
 export default function Footer() {
   return (
-    <footer>Footer</footer>
+    <footer className='mt-auto flex justify-between border-t border-white/10 h-16 px-3 sm:px-9 items-center text-xs text-white/25'>
+      <small className='text-xs'>&copy; 2050 Altacios. All Rights Reserved.</small>
+      <ul className='flex gap-x-3 sm:gap-x-8'>
+        {routes.map((route) => (
+          <li key={route.path}><Link href={route.path}>{route.name}</Link></li>
+        ))}
+      </ul>
+    </footer>
   )
 }
