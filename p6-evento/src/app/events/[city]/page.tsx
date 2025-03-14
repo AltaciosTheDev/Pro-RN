35bb6b86b23b418096af5c17fab54cf23a1b1e3f
AltaciosTheDev <enzo.altamirano98@gmail.com>
2025-03-13 20:54:12 -0600

    Evento Type

--- p6-evento/src/app/events/[city]/page.tsx
+++ p6-evento/src/app/events/[city]/page.tsx
@@ -1,17 +1,6 @@
 import H1 from '@/app/components/H1'
+import { Evento } from '@/lib/types'
 import React from 'react'
-
-// {
-//   id: 19,
-//   name: 'Dance Fusion Festival',
-//   slug: 'dance-fusion-festival',
-//   city: 'Austin',
-//   location: 'Austin Street Dance Studio',
-//   date: '2030-11-28T00:00:00.000Z',
-//   organizerName: 'Rhythm Revolution',
-//   imageUrl: 'https://images.unsplash.com/photo-1642178225043-f299072af862?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=100',
-//   description: 'Experience a blend of dance styles from around the world. Participate in dance workshops, watch electrifying performances, and dance the night away.'
-// }
 
 type Params = {
   city: string
@@ -25,7 +14,7 @@ export default async function EventsPage({params}: EventsPageProps) {
   const {city} = params
 
   const response = await fetch(`https://bytegrad.com/course-assets/projects/evento/api/events?city=${city}`)
-  const events = await response.json()
+  const events: Evento[] = await response.json()
   console.log(events)
 
   return (
