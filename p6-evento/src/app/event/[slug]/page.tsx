ad92e606997a7176bd3747b3269fc0d970b8b76b
AltaciosTheDev <enzo.altamirano98@gmail.com>
2025-03-15 13:01:01 -0600

    Event Page hero section structure and styling

--- p6-evento/src/app/event/[slug]/page.tsx
+++ p6-evento/src/app/event/[slug]/page.tsx
@@ -1,3 +1,4 @@
+import H1 from "@/app/components/H1"
 import { Evento } from "@/lib/types"
 import Image from "next/image"
 
@@ -17,7 +18,7 @@ export default async function EventPage({params}:EventPageProps) {
     console.log('------------------')
     console.log(event)
   return <main>
-    <section className="relative h-[361px] overflow-hidden">
+    <section className="relative overflow-hidden flex justify-center items-center">
       <Image
         className="object-cover blur-3xl z-0"
         fill
@@ -27,13 +28,22 @@ export default async function EventPage({params}:EventPageProps) {
         src={event.imageUrl}
         priority
       ></Image>
-      <div className="z-1 relative">
+      <div className="z-1 relative flex gap-6 lg:gap-16 flex-col lg:flex-row py-10 lg:py-20">
         <Image
           src={event.imageUrl}
           alt={event.name}
           width={300}
           height={201}
+          className="rounded-xl border-2 border-white/50 w-full"
         />
+        <div className="flex flex-col">
+          <p className="text-white/75">
+          {new Date(event.date).toLocaleDateString("en-US", {weekday: "long", month: "long", day:"numeric"})}
+          </p>
+          <H1 className="mb-2 mt-1 whitespace-nowrap lg:text-5xl">{event.name}</H1>
+          <p className="whitespace-nowrap text-xl">Organized by <span className="italic">{event.organizerName}</span></p>
+          <button className="bg-white/20 text-lg capitalize mt-5 lg:mt-auto py-2 border-2 border-white/10 rounded-md hover:scale-105 transition active:scale-[1.02]">Get tickets</button>
+        </div>
       </div>
     </section>
     <div>
